996aa32496dfcb4f49d69894064152d9811f75eb
HarryPerez <franco.perez@wolox.com.ar>
2019-02-11 15:41:17 -0300

    fix pr errors

--- app/models/user.js
+++ app/models/user.js
@@ -4,18 +4,14 @@ module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define(
     'user',
     {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER
-      },
       firstName: { alowNull: false, type: DataTypes.STRING },
       lastName: { alowNull: false, type: DataTypes.STRING },
       email: { alowNull: false, type: DataTypes.STRING, unique: true },
       password: { alowNull: false, type: DataTypes.STRING }
     },
-    {}
+    {
+      paranoid: true
+    }
   );
   user.associate = function(models) {
     // associations can be defined here
