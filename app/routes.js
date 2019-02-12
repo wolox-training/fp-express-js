27e3f41307de18e1569fca94e1bc335586a199e6
HarryPerez <franco.perez@wolox.com.ar>
2019-02-12 13:29:15 -0300

    add express-validator lib and checks

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,31 @@
+const { check, validationResult } = require('express-validator/check');
+
 const users = require('./controllers/users.js'),
-  emailValidator = require('./middlewares/emailValidator').handle,
-  passwordValidator = require('./middlewares/passwordValidator').handle,
-  userFieldsValidator = require('./middlewares/userFieldsValidator').handle;
+  userFieldsValidator = require('./middlewares/userFieldsValidator').handle,
+  errors = require('./errors');
 
 exports.init = app => {
-  app.post('/users', [userFieldsValidator, emailValidator, passwordValidator], users.create);
+  app.post(
+    '/users',
+    [
+      userFieldsValidator,
+      check('email')
+        .isEmail()
+        .withMessage('must be an email')
+        .matches(/[A-Z0-9._%+-]*@wolox.com.ar/)
+        .withMessage('must be @wolox.com.ar'),
+      check('password')
+        .isLength({ min: 8 })
+        .withMessage('must be at least 8 chars long')
+        .matches(/^[a-zA-Z0-9]+$/)
+        .withMessage('must have only letters and numbers')
+    ],
+    (req, res, next) => {
+      const validationErrors = validationResult(req);
+      if (!validationErrors.isEmpty()) {
+        throw errors.unprocessableEntity(validationErrors.array());
+      }
+      return users.create(req, res, next);
+    }
+  );
 };
