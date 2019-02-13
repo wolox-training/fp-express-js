2f08b3c7a2631f1551191c8936e92153ed54a984
HarryPerez <franco.perez@wolox.com.ar>
2019-02-13 16:27:38 -0300

    fix user tests

--- test/user.js
+++ test/user.js
@@ -32,4 +32,31 @@ describe('/users POST', () => {
         res.should.have.status(500);
         res.body.message.should.equal('The user with email: test@wolox.com.ar already exists');
       }));
+  it('should fail when the password is too short', () =>
+    chai
+      .request(server)
+      .post('/users')
+      .send({
+        firstName: 'test',
+        lastName: 'wolox',
+        email: 'test@wolox.com.ar',
+        password: '1234567'
+      })
+      .then(res => {
+        res.should.have.status(422);
+        res.body.message[0].msg.should.equal('must be at least 8 chars long');
+      }));
+  it('should fail when some field does not exist', () =>
+    chai
+      .request(server)
+      .post('/users')
+      .send({
+        lastName: 'wolox',
+        email: 'test@wolox.com.ar',
+        password: '1234567'
+      })
+      .then(res => {
+        res.should.have.status(422);
+        res.body.message[0].msg.should.equal('can not be empty');
+      }));
 });
